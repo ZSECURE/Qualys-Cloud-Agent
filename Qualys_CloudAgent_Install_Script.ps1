11b8512722a3c8ced94e71b801cc0a017c845415
Zak Clifford <46941177+ZSECURE@users.noreply.github.com>
2021-07-20 16:44:45 +0100

    Create Qualys_CloudAgent_Install_Script.ps1

---
+++ Qualys_CloudAgent_Install_Script.ps1
@@ -0,0 +1,70 @@
+# =============================================================================
+# File Name: Qualys_CloudAgent_Install_Script.ps1
+# =============================================================================
+# Name: Qualys Cloud Agent Install Script
+# Author: Zak Clifford 
+# Contact:  zak@zsecure.uk
+# Version 1.0
+# Created: 20 Jul 2021
+# Updated: N/A
+# Description: Checks for exisiting install of Qualys Cloud Agent then installs
+# Description: if it's not found
+# =============================================================================
+# Function Change Log
+# v1.0 - Creation of script
+# =============================================================================
+$ver = "1.0"
+
+# =============================================================================
+# START OF CODE
+# =============================================================================
+
+##Start of Script
+
+# Please Edit the Below
+$CustID = "Please enter the Customer ID"
+$ActiID = "Please enter the Activation ID"
+$WebURI = "Please enter the Web URI"
+
+# Enabling loggin for the script and saving to c:\windows\temp
+$ErrorActionPreference="SilentlyContinue"
+Stop-Transcript | out-null
+$ErrorActionPreference = "Continue"
+Start-Transcript -path C:\windows\temp\Qualys-Agent.txt -append
+
+# Setting script location as path
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+Set-Location -Path $dir
+
+$Qualysinstalled = $false
+$Qualysproc = $false
+
+$arguments = "CustomerId={" + $CustID + "} ActivationId={" + $ActiID + "} WebServiceUri=" + $WebURI
+
+# Installing the Qualys cloud agent
+Write-Host [+] Installing the Qualys cloud agent
+Write-Host [=]
+Write-Host [=]
+Start-Process -Wait -FilePath ".\QualysCloudAgent.exe" -ArgumentList $arguments -passthru 
+
+$Qualysinstalled = Test-Path -Path "C:\Program Files\Qualys\QualysAgent\QualysAgent.exe"
+$Qualysproc = Get-Process -Name QualysAgent -ErrorAction SilentlyContinue | Select ProcessName,Id
+if($Qualysproc -eq $null)
+{
+ $Qualysproc = $false
+}
+else
+{
+ $Qualysproc = $true
+}
+
+Write-Host [+] Qualys is installed at C:\Program Files\Qualys\QualysAgent\QualysAgent.exe : $Qualysinstalled
+Write-Host [=]
+Write-Host [=]
+Write-Host [+] Qualys is running : $Qualysproc
+Write-Host [=]
+Write-Host [=]
+Stop-Transcript
+
+# End of Script
